6130c9c34ccd8bafc794bcc65273a02c8305da0f
Max Miller <50274422+MaxNMiller@users.noreply.github.com>
2023-11-10 12:21:19 -0500

    Create Solution.java

---
+++ CQ7/RunningTime/Solution.java
@@ -0,0 +1,60 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'runningTime' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static int runningTime(List<Integer> arr) {
+        int size = arr.size(); //size of array
+        int s = 0;  // number of shifts
+
+        for (int i = 1; i < size; i++) {
+            int position = i - 1;
+            int temp = arr.get(i);
+
+            while (position >= 0 && arr.get(position) > temp) {
+                arr.set(position + 1, arr.get(position));
+                s++;
+                position--;
+            }
+            arr.set(position + 1, temp);
+        }
+        return s;
+    }
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        int result = Result.runningTime(arr);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
